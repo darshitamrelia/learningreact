b2ab7252868918553830fd69d9fcba1394a68ab1
Darshit <rohan19022@gmail.com>
2023-09-05 12:39:19 +0530

    comment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
-import DataTable from "./DataTable";
-import { Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import Create from "./Create";
-import Update from "./Update";
+import Counter from "./Counter";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/update/:id" element={<Update />} />
-      </Routes>
+      <Counter />
     </div>
   );
 }
